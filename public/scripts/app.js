d2bddae756d5b1f6b8c4bae373b15c93bdc1d0ba
Aliabbas Merchant <merchantaliabbas@gmail.com>
2020-01-18 02:57:00 +0530

    Connection to server done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+const testing = true;
+
+$(() => {
+    $("#controller").hide();
+    const connectionSecret = testing ? "abcde" : Array(5).fill(0).map(_x => Math.random().toString(36).charAt(2)).join('');
+    document.getElementById("secretText").value = connectionSecret;
+    $("#connectButton").click(() => {
+        let url = testing ? "http://127.0.0.1:3124" : document.getElementById("connectInput").value;
+        $.post(`${url}/connect`, { connectionSecret })
+            .done((data) => {
+                data = JSON.parse(data)
+                if(data.ok) {
+                    $("#connector").hide();
+                    $("#controller").show();
+                } else {
+                    alert(data.msg)
+                }
+            }).fail((err) => {
+                console.log("err", err);
+                alert("Sorry, an error occurred");
+            });
+    });
+})
