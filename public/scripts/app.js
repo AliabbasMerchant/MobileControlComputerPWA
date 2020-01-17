40c8e27439e10f8e0106924946ec2e13069f04db
Aliabbas Merchant <merchantaliabbas@gmail.com>
2020-01-18 04:45:29 +0530

    A few UI changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,13 +16,24 @@ $(() => {
                     console.log("err", err);
                 });
         });
+        document.getElementById("trackpad").ontouchmove = (event) => {
+            var x = event.touches[0].clientX;
+            var y = event.touches[0].clientY;
+            console.log(x, y)
+        };
+        document.getElementById("trackpad").ontouchstart = (event) => {
+            console.log("start")
+        };
+        document.getElementById("trackpad").ontouchend = (event) => {
+            console.log("end")
+        };
     }
 
-    $("#controller").hide();
     const connectionSecret = testing ? "abcde" : Array(5).fill(0).map(_x => Math.random().toString(36).charAt(2)).join('');
     document.getElementById("secretText").value = connectionSecret;
     $("#connectButton").click(() => {
-        URL = testing ? "http://127.0.0.1:3124" : document.getElementById("connectInput").value;
+        // URL = testing ? "http://127.0.0.1:3124" : document.getElementById("connectInput").value;
+        URL = testing ? "http://192.168.43.199:3124" : document.getElementById("connectInput").value;
         $.post(`${URL}/connect`, { connectionSecret })
             .done((data) => {
                 data = JSON.parse(data)
