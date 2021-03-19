49a85a8f5329af3cc23798a6030a45115ed5d507
Harsh Chheda <harshkc0132@gmail.com>
2021-03-19 20:07:37 +0530

    Screen share functionality added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 const testing = true;
 let URL;
+let socket;
 
 $(() => {
     const connectionSecret = testing ? "abcde" : Array(5).fill(0).map(_x => Math.random().toString(36).charAt(2)).join('');
@@ -17,6 +18,7 @@ $(() => {
             console.log("err", err);
         });
     }
+
     function trackpadEventHandler(event) {
         var trackpad = $('#trackpad')
         var offset = trackpad.offset();
@@ -32,6 +34,7 @@ $(() => {
             let signal = $(e.target).data('signal');
             send(signal, 'type');
         });
+
         document.getElementById("trackpad").ontouchmove = trackpadEventHandler;
         document.getElementById("trackpad").ontouchstart = trackpadEventHandler;
         document.getElementById("textarea").oninput = (event) => {
@@ -45,6 +48,44 @@ $(() => {
                 send('delete', 'type')
             }
         };
+
+        $("#startButton").click(() => {
+            
+            socket = io.connect('http://localhost:3124');
+            
+            socket.on('after connect', function(msg) {
+                console.log(msg.data);
+            });
+        
+            socket.emit('Start Recieving Data', 'Get the image data');
+
+            socket.on('data', function(data) {
+                new_data = data['msg']['pixels']
+                var binary = '';
+                var bytes = new Uint8Array(new_data);
+                var len = bytes.byteLength;
+                for (var i = 0; i < len; i++) {
+                    binary += String.fromCharCode( bytes[ i ] );
+                }
+                var encoded_string = window.btoa(binary);
+
+                console.log('Received: ' + encoded_string);
+                document.getElementById('image')
+                .setAttribute(
+                    'src', 'data:image/png;base64,' + encoded_string
+                );
+            });
+        });
+
+        $("#endButton").click(() => {
+            socket.emit('Close connection', 'Closing the connection from client side')
+            socket.close()
+            console.log("Ended")
+            document.getElementById('image').setAttribute(
+                'src', ''
+            );
+        });
+
         $(".mouseButton").click((e) => {
             let signal = $(e.target).data('signal');
             send(signal, 'click');
